877e8c02e9b1a26dd89ae7e465f0e0360481f88f
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-11 21:10:18 +0700

    if expression, welcom js

---
+++ scripts/if-expression.js
@@ -0,0 +1,11 @@
+        const examValue = 66;
+
+        if (examValue > 80){
+            document.writeln("<p>Your Grade is A</p>")
+        } else if(examValue >70 ){
+            document.writeln("<p>Your Grade is B</p>")
+        } else if(examValue >65 ){
+            document.writeln("<p>Your Grade is C</p>")
+        } else {
+            document.writeln("<p>Try Again</p>")
+        }
