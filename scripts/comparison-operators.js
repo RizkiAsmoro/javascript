3d40ebe469219af68a557b01afbc2819b5d8bcf3
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-11 21:21:12 +0700

    comparison and math operators js

---
+++ scripts/comparison-operators.js
@@ -0,0 +1,24 @@
+ //Semua hasil operator perbandingan adalah hasilnya boolean (True / False)
+ let result = 5 =="5";
+ document.writeln("<p>" + result + "</p>");
+
+ result = 5 === "5";
+ document.writeln("<p>" + result + "</p>");
+
+ result = 5 > 10;
+ document.writeln("<p>" + result + "</p>");
+
+ result = 5 < 10;
+ document.writeln("<p>" + result + "</p>");
+
+ result = 5 !== "5";
+ document.writeln("<p>" + result + "</p>");
+
+ result = 5 != "5";
+ document.writeln("<p>" + result + "</p>");
+
+ result = 5 <= 10;
+ document.writeln("<p>" + result + "</p>");
+
+ result = 5 >= 10;
+ document.writeln("<p>" + result + "</p>");
---
+++ scripts/math-operators.js
@@ -0,0 +1,10 @@
+let result = 1 + 3;
+document.writeln("<p> 1 + 3 = " + result + "</p>");
+let originalResult = result;
+
+result = result * 2;
+document.writeln("<p>" + originalResult + " x 2 = " + result +"</p>");
+originalResult = result;
+
+result = result - 3
+document.writeln("<p>" + originalResult + " - 3 = " + result +"</p>")
