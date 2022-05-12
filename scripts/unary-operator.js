a57a437a9689c75bd46b775e3df3eb4bedf9c8d6
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-12 13:21:47 +0700

    unary operator

---
+++ scripts/unary-operator.js
@@ -0,0 +1,12 @@
+//unary digunakan untuk operasi 1 data saja
+let result = +1; // nilai default sebenarnya positif jadi bisa saja ditulis 1
+document.writeln("<p>" + result + "</p>")
+
+result++;//menaikan nilai 1 angka, bisa juga ditulis result=result+1 atau result +=1
+document.writeln("<p>" + result + "</p>")
+
+result--;//menurunkan nilai 1 angka, bisa juga ditulis result=result-1 atau result -=1
+document.writeln("<p>" + result + "</p>")
+
+result = -result;// mengubah nilai menjadi negative
+document.writeln("<p>" + result + "</p>")
