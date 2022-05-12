cd71cb9de5efaa1760d641e621b5d5708a403c78
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-12 13:09:36 +0700

    console and logic operators

---
+++ scripts/console.js
@@ -0,0 +1,14 @@
+        /*
+        - Print tulisan bukan di area HTML atau bisa disebut logging.
+        - Logging adalah menampilkan informasi dari aplikasi yang sedang berjalan tanpa mengganggu alur kerja aplikasi
+        - text nya tidak tampil di HTML jadi dapat dilihat dengan cara klik kanan-inspect-tab Console
+        - console.debug ada pada dafuault level Verbose
+        */
+    
+        console.info("This is information console")
+
+        console.warn("This is warning console")
+
+        console.error("This is error console")
+
+        console.debug("This is debug console")
