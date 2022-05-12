64efb100a0aa7a59c54b0def52f12c65f621cdce
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-12 13:05:27 +0700

    augmented assignment operator

---
+++ scripts/augmented-assignment-operator.js
@@ -0,0 +1,10 @@
+let result = 1 + 3;
+document.writeln("<p> 1 + 3 = " + result + "</p>");
+let originalResult = result;
+
+result *= 2; // sama saja result = result * 2
+document.writeln("<p>" + originalResult + " x 2 = " + result +"</p>");
+originalResult = result;
+
+result -= 3 // sama saja result = result - 3
+document.writeln("<p>" + originalResult + " - 3 = " + result +"</p>")
