89aa69c3206dccef594e7c05ef8116b1c41bb334
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-13 14:44:11 +0700

    in operator

---
+++ scripts/in-operator.js
@@ -0,0 +1,34 @@
+/*
+- Operator untuk mengecek ada properti atau tidak pada suatu object
+- Untuk cek index di array ada atau tidak
+*/
+
+const siswa = {
+    nama : "Rizki",
+    kelas : 17
+}
+if ("nama" in siswa){// true
+    alert (`Hello ${siswa.nama}`);
+}else {
+    alert("Hello")
+}
+
+/*
+- In Operator hanya mengecek apakah sebuah properti index ada atau tidak, jadi tidak mengecek value nya
+- walaupun nilai propertinya undefine atau null maka dianggap ada
+*/
+
+const karyawan = {
+    nama : "Asmoro",
+    umur : undefined
+}
+if ("umur" in karyawan){
+    alert(`Hello ${karyawan.umur}`);
+}else{
+    alert("Hello")
+}
+
+//selama index nya ada maka hasilnya akan true
+const nama = [null, "rizki", "Asmoro"];
+const result = 0 in nama;
+document.writeln(`${result}`)
