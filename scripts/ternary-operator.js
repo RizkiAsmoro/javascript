2648ae682dbee736bb947b7aadcfcd702232154f
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-17 14:37:41 +0700

    comment on ternary

--- scripts/ternary-operator.js
+++ scripts/ternary-operator.js
@@ -1,6 +1,7 @@
 /*
 - Adalah operasi sederhana dari IF statement
 - Terdiri dari kondisi yang dievaluasi, jika menghasilkan nilai TRUE maka nilai pertama yang diambil, jika FALSE maka nilai kedua yang diambil
+- Jika hanya ada dua kondisi maka lebih sederhana menggunakan ternary operator dibandingkan IF Operator
 */
 
 // Berikut adalah contoh menggunakan Operator IF
