97979f2e0bfe6e88b0a0bbd12db8fe9e9c200e53
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-17 16:33:50 +0700

    optional chaining

---
+++ scripts/optional-chaining.js
@@ -0,0 +1,25 @@
+/*
+- Optional Chaining Operator (?) digunakan untuk mengamankan ketika ingin mengakses proprty sebuah data nullish
+- Jika mencoba akses property dari sebuah object dari nullish tanpa menggunakan optional chaining maka hasilnya error
+*/
+
+
+const student = {};
+let student_1;
+
+// pengecekan menggunakan IF
+if (student.address !== undefined && student.address !== null){
+    student_1 = student.address.country;
+}
+document.writeln("Data Success");
+document.writeln(`<p>${student_1}</p>`)
+
+// pengecekan menggunakan Optional Chaining
+let student_2 = student?.address?.country;
+document.writeln("Data Success");
+document.writeln(`<p>${student_1}</p>`)
+
+/*
+- Dari kedua pengecekan diatas maka Optional Chaining lebih efektif dalam penulisan codingnya
+- Hasil kedua sama-sama undefine karena property country tidak ada
+ */
