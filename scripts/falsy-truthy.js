e73284ec89e9661d0f64e69ebb7be19b512e068b
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-18 10:14:38 +0700

    falsy-truthy

---
+++ scripts/falsy-truthy.js
@@ -0,0 +1,30 @@
+/*
+Falsy
+- Dalam konteks boolean dianggap false
+- Pada JavaScript kondisi tidak hanya boolean yang true/false tapi diluar boolean pun bisa
+Contoh data Falsy :
+ 1. false       =  dianggap false
+ 2. 0, -0       =  dianggap false
+ 3. "",'',``    = semua string kosong dianggap false
+ 4. null        = null dianggap false
+ 5. undefined   = dianggap false
+ 6. NaN         = Not a Number dianggap false
+ 
+ Truthy
+ - Adalah kebalikan dari false
+*/
+
+const value = "";// String kosong maka yang muncul false
+
+if(value){
+    document.writeln("<p>True</p>");
+}else{
+    document.writeln("<p>False</p>");
+}
+
+const data = "False";// String tidak kosong maka hasilnya true
+if(data){
+    document.writeln("<p>True</p>");
+}else{
+    document.writeln("<p>Fasle</p>");
+}
