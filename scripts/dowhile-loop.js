dce27ceeafbe3c6c71ccb6396d5b3341cc4b1982
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-18 12:57:39 +0700

    do while loop

---
+++ scripts/dowhile-loop.js
@@ -0,0 +1,11 @@
+/*
+- Mirip dengan perulangan while perbedaan hanya pada pengecekan condition
+- Pengecekan condition dilalukan setelah perulangan, jika true lanjut jika false berhenti
+- Jadi minimum ada 1x perulangan walaupun condition false
+*/
+
+let x = 1;
+do {
+    document.writeln(`<p>do while loop ${x}</p>`)
+    x++;
+}while(x <=5);
