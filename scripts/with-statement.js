105c28128439226058af50603e9a8857a23ccee8
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-19 14:43:44 +0700

    with statement

---
+++ scripts/with-statement.js
@@ -0,0 +1,20 @@
+/*
+- With Statement digunakan untuk menurunkan scope data
+- Dapat digunakan untuk mengakses property tanpa menyebut datanya atau objectnya
+*/
+
+const name ={
+    firstName : "Rizki",
+    middleName : "Wilianto",
+    lastName : "Asmoro"
+};
+
+with(name){
+    console.info(firstName);
+    console.info(middleName);
+    console.info(lastName);
+}
+
+/* With Statement tidak direkomendasikan untuk digunakan 
+karena dapat membuat ambigu jika terdapat property yang sama
+*/
