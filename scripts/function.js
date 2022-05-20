18215c5ad8539ec386f89dc260d5819accc2f57d
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-20 11:07:07 +0700

    update

---
+++ scripts/function.js
@@ -0,0 +1,17 @@
+/*
+Function
+- Adalah kode program yang berjalan saat kita panggil
+- Cara membuatnya menggunakan kata kunci function lalu diikuti dengan nama function,
+  lalu (), dan diakhiri dengan block kurung {}
+- Memanggil function dengan menggunakan nama function lalu diikuti dengan kurung ()
+- FUNCTION disebut juga METHOD
+- Kita bisa mengirim informasi ke function yang ingin kita panggil dengan menggunakan parameter / argumen
+- Parameter ditempatkan didalam kurung () di deklarasi function
+- Parameter bisa lebih dari 1 dan dipisahkan dengan koma dan tidak perlu dideclare seperti let, const, atau var
+*/
+
+function perkalianDuaNilai(x,y,z){//membuat function
+    z = x * y
+    document.writeln(`${x} x ${y} = ${z}`);
+};
+perkalianDuaNilai (x= 10,y= 5)//memanggil function
--- scripts/with-statement.js
+++ scripts/with-statement.js
@@ -3,6 +3,16 @@
 - Dapat digunakan untuk mengakses property tanpa menyebut datanya atau objectnya
 */
 
+
+//jika tidak menggunakan with statement maka mengakses object nya sepert berikut
+console.info(name.firstName);
+console.info(name.middleName);
+console.info(name.lastName);
+// jika objectnya banyak maka akan menjadi tidak efektif tanpa menggunakan with statement
+
+
+
+// Berikut menggunakan with statement
 const name ={
     firstName   : "Rizki",
     middleName  : "Wilianto",
@@ -14,14 +24,6 @@ with(name){
     console.info(middleName);
     console.info(lastName);
 }
-
 /* With Statement tidak direkomendasikan untuk digunakan 
 karena dapat membuat ambigu jika terdapat property yang sama
 */
-
-//jika tidak menggunakan with statement maka mengakses object nya sepert berikut
-
-console.info(name.firstName);
-console.info(name.middleName);
-console.info(name.lastName);
-// jika objectnya banyak maka akan menjadi tidak efektif tanpa menggunakan with statement
