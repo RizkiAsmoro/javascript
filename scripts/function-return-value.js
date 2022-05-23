537e6f922e41e04ee19774f242d761fa8186f077
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-23 14:12:20 +0700

    update - Function return value

--- scripts/function-return-value.js
+++ scripts/function-return-value.js
@@ -6,3 +6,43 @@ Function Return Value
 - Function hanya mengembalikan 1 value, jika ingin lebih dari 1 makan value tsb dibungkus menggunakan array
 */
 
+//Contoh return 1
+function name (firstName, lastName){
+    const x = (`Hello ${firstName} ${lastName}`);
+    return x;
+}
+
+const z = name ("Rizki", "Asmoro");
+document.writeln(`${z}`);
+
+
+//Contoh return lebih dari 1 kali
+function getFinalExam (value){
+    if(value >=90){
+        return "A";
+    }else if (value >=80){
+        return "B";
+    }else if (value >= 70){
+        return "C";
+    }else{
+        return "D";
+    }
+}
+const finalValue = getFinalExam(99);
+document.writeln(`<p>Your Final Exam Value ${finalValue}</p>`);
+
+
+// Return dapat digunakan untuk menghentikan eksekusi sebuah function
+function isContains(array, searchValue){
+    for (const element of array){
+        document.writeln(`<p>index ${element}</p>`)
+        if (element === searchValue){
+            return true;//jika nilai yang dicari ada
+        }
+    }
+    return false;//jika nilai yang dicari tidak ada
+}
+const array = [12,2,23,4,32,1124,24,13,234,346,234];//data aray
+const search = 24;//nilai yang dicari
+const found = isContains(array, search);
+document.writeln(`<p>search value = ${search} ${found}</p>`);
