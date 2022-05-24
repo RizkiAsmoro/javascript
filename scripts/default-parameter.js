4ebf8eae595a2ae4d1c4564e4bb8339b32894f97
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-24 11:54:14 +0700

    default parameter

---
+++ scripts/default-parameter.js
@@ -0,0 +1,11 @@
+      /*
+Default Parameter
+- Ketika parameter tidak diberi data/default value maka secara otomatis patameter bisa diisi default value
+*/
+
+function donatur (alamat, nama = "Hamba ALLAH SWT"){
+    console.log(`Nama   : ${nama}`);
+    console.log(`Alamat : ${alamat}`); 
+} 
+donatur("Jakarta");// parameter yang diisi hanya alamat maka default parameter nama = Hamba Allah SWT
+    
