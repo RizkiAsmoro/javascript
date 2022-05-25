92c53727f071f71291428d278ae5e88f334e43d4
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-25 11:15:33 +0700

    update spread syntax

--- scripts/rest-parameter.js
+++ scripts/rest-parameter.js
@@ -2,7 +2,7 @@
 rest parameter
 - Fitur digunakan untuk mengirim data ke parameter sebanyal-banyaknya
 - Secara otomatis datanya akan dikonversikan menjadi array
-- Hanya ada 1 di function
+- Hanya boleh 1 di function
 - Posisinya hanya boleh di posisi paling akhir
 */
 function x (text, ...data){///titik 3 kali adalah rest parameter
@@ -10,7 +10,16 @@ function x (text, ...data){///titik 3 kali adalah rest parameter
     for(const y of data){
         total += y;
     }
-    document.writeln(`${text} ${total}`)
+    document.writeln(`<p>${text} ${total}</p>`)
 }
+x ("Nilai Pertama :",10,20,30,40,50);
+x ("Nilai Kedua :",60,70,80,90)
 
-x ("Nilai",10,20,30,40,50);
+/*
+Jika data sudah terlanjut berupa array pada Rest Parameter
+maka dapat menggunakan fitur Spread Syntax berupa titik 3x didepan arraynya
+maka array tsb diubah ke rest parameter
+Contohnya sebagai berikut
+*/ 
+const data = [5,5,5,5,5];
+x("Spread Syntax :", ...data);//nilai data yang berupa array akan diubah
