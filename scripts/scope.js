eae8858b60dc3cb85319aed3f43d6f69fc2ca658
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-27 13:38:28 +0700

    global and local scope

--- scripts/scope.js
+++ scripts/scope.js
@@ -6,14 +6,17 @@ Scope (Global dan local)
 - Data di local scope hanya bisa diakses di local scope tersebut atau di scope local dibawahnya
 */
 
+// Contoh mengakses global scope
 const x = 2// global scope variable
 function kali (y){//local scope kali
-    hasil_kali = y * x;
-    document.writeln(`<p>Hasil perkalian ${hasil_kali}</p>`);
+    hasil_kali = y * x; 
 }
 function bagi (z){//local scope bagi
     hasil_bagi = z / x;
-    document.writeln(`<p>Hasil pembagian ${hasil_bagi}</p>`);
 }
-kali(7);
-bagi(20);
+kali(7);//mengakses global scope
+bagi(20);//mengakses global scope
+document.writeln(`<p>Hasil pembagian ${hasil_bagi}</p>`);//mengakses global scope
+document.writeln(`<p>Hasil perkalian ${hasil_kali}</p>`);//mengakses global scope
+
+//
