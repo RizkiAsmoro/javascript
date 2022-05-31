0a9ebe52bcf2e5fa029da7c41a77073aee9c3161
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-05-31 14:35:44 +0700

    update arrow function

--- scripts/function-arrow.js
+++ scripts/function-arrow.js
@@ -19,3 +19,14 @@ sayHi("Asmoro");
 //Contoh ke 3 tanpa block function
 const sum = (a, b) => a + b;
 document.writeln(sum(10,20));
+
+
+//Contoh ke 4 tanpa kurung parameter / parameter hanya 1
+const sayName = myname => document.writeln(`<p>${myname}</p>`);
+sayName("Alkha");
+
+//Contoh ke 5, Arrow Function sebagai parameter seperti anonymous function
+function callMyName(call){
+    call("Maezurra");
+}
+callMyName(myName => document.writeln(`<p>${myName}</p>`))
