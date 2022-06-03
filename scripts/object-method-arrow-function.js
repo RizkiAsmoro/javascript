058f3d582c1371abe9af3743fb06e811e8e42cca
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-06-03 15:05:22 +0700

    arrow function in object method

---
+++ scripts/object-method-arrow-function.js
@@ -0,0 +1,18 @@
+/*
+Arrow Function pada Object Method
+- Arrow Function bisa digunakan sebagai object method
+- Arrow function tidak bisa digunakan untuk akses argumen obejct,
+  function generator, kata kunci this dan kata kunci super
+*/
+
+//Contoh 1
+const person = {
+    name:"rizki",
+    sayHello: (name) =>{
+        console.info(this);//Window
+        console.info(`Hello ${name} my name is ${this.name}`)//this tidak dapat digunakan
+        console.info(`Hello ${name}`)
+
+    }
+};
+person.sayHello("Asmoro")
