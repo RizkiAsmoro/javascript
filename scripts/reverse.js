4c101e9386be6b9a3944e75753454c46ada37358
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-07-06 17:21:16 +0700

    reverse

---
+++ scripts/reverse.js
@@ -0,0 +1,11 @@
+/*
+Reverse:
+- Membalik suatu nilai secara urutan
+*/
+
+//Reverse Aray
+console.info(`----Reverse Aray----`)
+const angka = [1,2,3,4,5];
+console.info(`Urutan angka ${angka}`)
+angka.reverse()
+console.info(`Reverse angka ${angka}`)
