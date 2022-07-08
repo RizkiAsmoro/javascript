48ca7c6c5afc364b98ee2ff4575a12b921e2b685
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-07-08 16:29:07 +0700

    split

---
+++ scripts/split.js
@@ -0,0 +1,15 @@
+/*
+Split :
+*/
+
+//Split String
+    const nama = "Rizki Wilianto Asmoro";
+    const s = nama.split(" ");
+    const s1 = nama.split("");
+    const s2 = nama.split(" ",2);
+
+
+    console.info(`${s}`)// Rizki,Wilianto,Asmoro
+    console.info(`${s1}`)// R,i,z,k,i, ,W,i,l,i,a,n,t,o, ,A,s,m,o,r,o
+    console.info(`${s2}`)// Rizki,Wilianto
+    console.info(`${s[2]}`)// Asmoro
