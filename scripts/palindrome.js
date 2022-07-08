844d603e5fc95925071726f54410b0e0dc3e9823
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-07-08 15:25:43 +0700

    reverse array and string

---
+++ scripts/palindrome.js
@@ -0,0 +1,10 @@
+/*
+Palindrome:
+
+*/
+
+//palindrome array
+
+const angka = [1,2,3,4,5,6,7,8,9,0];
+rev = angka.reverse()
+console.info(`${rev}`)
--- scripts/reverse.js
+++ scripts/reverse.js
@@ -5,7 +5,16 @@ Reverse:
 
 //Reverse Aray
 console.info(`----Reverse Aray----`)
-const angka = [1,2,3,4,5];
+const angka = [1,2,3];
 console.info(`Urutan angka ${angka}`)
-angka.reverse()
-console.info(`Reverse angka ${angka}`)
+angka.reverse();
+console.info(`Reverse Angka ${angka}`)
+
+
+// Reverse String
+function reverse(s) {
+    return s.split("").reverse().join("");
+    
+  }
+  const x = reverse("rizki");
+  console.info(`${x}`);
