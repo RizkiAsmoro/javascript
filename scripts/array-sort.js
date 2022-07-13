7bebf62bf1aec11b4b213b330aa3299508c27d46
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-07-13 18:34:10 +0700

    array function sort

--- scripts/array-sort.js
+++ scripts/array-sort.js
@@ -3,17 +3,32 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
 Sort Array:
 - Secara default jika menggunakan method sort() maka array akan diurutkan secara ascending
 */
-
-let numbers = [5,4,7,9,2,8,3,1,6,0];
-numbers.sort()
-console.info(`${numbers}`)
-
-let name = ["Jaka","Kaka","Alex","Beckham"];
-name.sort();
-console.info(`${name}`)
-
+{
+    let numbers = [5,4,7,9,2,8,3,1,6,0];
+    numbers.sort()
+    console.info(`${numbers}`)
+}
+{
+    let name = ["Jaka","Kaka","Alex","Beckham"];
+    name.sort();
+    console.info(`${name}`)
+}
 // Jika array angka berupa string maka akan diurutkan berdasarkan character pertaman sesuai unicode
+{
+    let angka = ["40","9","30","80"];
+    angka.sort();
+    console.info(`${angka}`); //30,40,80,9
+}
 
-let angka = ["40","9","30","80"];
-angka.sort();
-console.info(`${angka}`); //30,40,80,9
+//Compare function
+{
+    console.info(`--------Compare Function--------`)
+    let numbers = [5,4,7,9,2,8,3,1,6,0];
+    numbers.sort((a,b)=>b-a)//sort descending menggunakan arrow function comparation
+    console.info(`${numbers}`)
+}
+// {
+//     let name = ["Jaka","Kaka","Alex","Beckham"];
+//     name.sort((a,b)=>a-b);
+//     console.info(`${name}`)
+// }
