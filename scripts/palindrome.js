3cfc82bc1297ea0e66cfd377ec468626d02c7dfd
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-07-13 15:25:27 +0700

    remark code

--- scripts/palindrome.js
+++ scripts/palindrome.js
@@ -5,10 +5,10 @@ Palindrome:
 
 //palindrome array
 
-const angka = [1,2,3,4,3,2,1,2];
-// const [... n] = angka;
-console.info(`${angka}`)
-const balik = angka.reverse()
+// const angka = [1,2,3,4,3,2,1,2];
+// // const [... n] = angka;
+// console.info(`${angka}`)
+// const balik = angka.reverse()
 
-const result = angka === balik
-console.info(`${result}`)
+// const result = angka === balik
+// console.info(`${result}`)
