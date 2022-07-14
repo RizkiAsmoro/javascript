5a056fcd567788b93dfb2ab56cd4171c46b5a907
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-07-14 18:59:14 +0700

    check odd event number

---
+++ scripts/odd-even-number.js
@@ -0,0 +1,24 @@
+/*
+Odd and Even number 
+*/
+// function isContains(array, searchValue){
+//     for(const element of array){
+//         if(element == searchValue){
+//             document.writeln(`${}`)    
+//         }
+//     }
+// return false;
+// }
+
+// const array = [1,2,3,4,5,6,7,8,9];
+// const search = array % 2 == 0;
+// const found = isContains(array, search);
+// // document.writeln(`<p>${found}</p>`)
+
+
+const number = 13;
+if (number % 2 == 0){
+    document.writeln(`<p>${number} is even number</p>`)
+}else{
+    document.writeln(`<p>${number} is odd number</p>`)
+}
