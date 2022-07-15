8f6f89f72af0b08a0a4ee56a8fd9badde199bf88
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-07-15 17:37:14 +0700

    separate odd and even number from array

--- scripts/odd-even-number.js
+++ scripts/odd-even-number.js
@@ -29,10 +29,10 @@ const odd = array.filter(num => {return num % 2 !== 0;});
 document.writeln(`${odd} is odd number`);
 
 
-// Memisahkan ganjil dan genap dari suatu array
+// Memisahkan ganjil dan genap dari suatu array dan di simpan ke array lain untuk angka genap dan ganjil
 const angka = [1,2,3,4,5,6,7,8,9];
-const genap = [];
-const ganjil = [];
+const genap = [];//untuk menampung angka genap
+const ganjil = [];//untuk menampung angka ganjil
 
 angka.forEach(n =>{
     if (n % 2 == 0) {
@@ -43,3 +43,19 @@ angka.forEach(n =>{
 })
 document.writeln(`<p>Angka genap :${genap}</p>`);
 document.writeln(`<p>Angka ganjil :${ganjil}</p>`);
+
+
+
+// Menentukan mana angka yang ganjil dan mana angka yang genap dari element suatu array
+const arrNum = [11,13,12,34,56,789,13,34];
+
+arrNum.forEach(n =>{//pada setiap element n di array arrNum
+    if (n % 2 == 0) {
+        // genap1.push(n)//push nilai n ke array genap
+        document.writeln(`<p>Angka genap :${n}</p>`);
+    }else{
+        // ganjil1.push(n)//push nilai n ke array ganjil
+        document.writeln(`<p>Angka ganjil :${n}</p>`);
+    }
+})
+
