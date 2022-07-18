4c64986189ca4caf4b7e8d19b0a66797f67f63d7
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-07-18 18:05:37 +0700

    cek palindrome string

--- scripts/palindrome.js
+++ scripts/palindrome.js
@@ -1,15 +1,15 @@
 /*
-Palindrome:
+Palindrome : adalah data yang dibaca dari depan dan belakang akan sama
 
 */
 
-// Palindrome String
+// Palindrome String menggunakan reverse
 
 let data = "kodOK"
 let data2 = "Katak"
 let data3 = "rizki" 
-let data4 = ["KOTAK"]
 
+//Contoh 1 (cek palindrome string)
 function checkPlindrome(n){
     let a = n.toLowerCase().split('');//ubah data menjadi uppercase dan split string
     let b = a.join(); // join string yang sudah diubah dan di split sebelumnya
@@ -24,4 +24,14 @@ function checkPlindrome(n){
 checkPlindrome(data);
 checkPlindrome(data2);
 checkPlindrome(data3);
-checkPlindrome(data4);
+
+
+// Contoh 2 (cek palindrome string)
+{
+    function isPalindrome(str){
+        return str.toLowerCase() == str.toLowerCase().split('').reverse().join(''); 
+    }
+
+    let n = isPalindrome('12321')
+    console.info(n)
+}
