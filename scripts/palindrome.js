8fcf6ad4827989fd0a98532a46bf874dadb1526d
RizkiAsmoro <rizkiasmoro@gmail.com>
2022-07-18 15:21:59 +0700

    update description

--- scripts/palindrome.js
+++ scripts/palindrome.js
@@ -5,18 +5,23 @@ Palindrome:
 
 // Palindrome String
 
-let data = "kodOK" 
+let data = "kodOK"
+let data2 = "Katak"
+let data3 = "rizki" 
+let data4 = ["KOTAK"]
 
 function checkPlindrome(n){
-    let a = n.toLowerCase().split('');
-    let b = a.join();
-    let reverseArray = [...b].reverse().join('');
+    let a = n.toLowerCase().split('');//ubah data menjadi uppercase dan split string
+    let b = a.join(); // join string yang sudah diubah dan di split sebelumnya
+    let reverseArray = [...b].reverse().join('');//reverse dan join string
 
     if(b === reverseArray){
-        return true;
+        console.info(`${n} is Palindrome`);
     }else{
-        return false;
+        console.info(`${n} is Not Palindrome`);
     }
 }
-console.info(checkPlindrome(data))
-
+checkPlindrome(data);
+checkPlindrome(data2);
+checkPlindrome(data3);
+checkPlindrome(data4);
